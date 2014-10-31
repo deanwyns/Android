a13950c8dcee737fd4290d1999dadf9ecbfd5c69
JaimySmets <jaimy.smets.s0645@student.hogent.be>
2014-10-31 14:11:11 +0100

    Revert "Revert "Vakantie Model""
    This reverts commit 3dedbd01eeaeb878a1e0f59917901571b40d4d92.

--- app/src/main/java/hogent/hogentprojecteniii_groep10/models/Vacation.java
+++ app/src/main/java/hogent/hogentprojecteniii_groep10/models/Vacation.java
@@ -1,14 +1,44 @@
 package hogent.hogentprojecteniii_groep10.models;
 
 public class Vacation {
+
+    private long id;
     private String title;
     private String description;
-    private int signups;
+    private String promoText;
+    private String location;
+    private int ageFrom;
+    private int ageTo;
+    private String transportation;
+    private int maxParticipants;
+    private double baseCost;
+    private double oneBmMemberCost;
+    private double twoBmMemberCost;
+    private boolean taxDeductable;
 
-    public Vacation(String title, String description, int signups) {
+    public Vacation(long id, String title, String description, String promoText, String location, int ageFrom, int ageTo, String transportation, int maxParticipants, double baseCost, double oneBmMemberCost, double twoBmMemberCost, boolean taxDeductable) {
+        this.id = id;
         this.title = title;
         this.description = description;
-        this.signups = signups;
+        this.promoText = promoText;
+        this.location = location;
+        this.ageFrom = ageFrom;
+        this.ageTo = ageTo;
+        this.transportation = transportation;
+        this.maxParticipants = maxParticipants;
+        this.baseCost = baseCost;
+        this.oneBmMemberCost = oneBmMemberCost;
+        this.twoBmMemberCost = twoBmMemberCost;
+        this.taxDeductable = taxDeductable;
+    }
+
+    //"Basic" constructor
+    public Vacation(long id, String title, String description, String promoText, String location) {
+        this(id, title, description, promoText, location, 0, 0, null, 0, 0.0, 0.0, 0.0, false);
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getTitle() {
@@ -19,7 +49,41 @@ public class Vacation {
         return description;
     }
 
-    public int getSignups() {
-        return signups;
+    public String getPromoText() {
+        return promoText;
     }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getAgeFrom() {
+        return ageFrom;
+    }
+
+    public int getAgeTo() {
+        return ageTo;
+    }
+
+    public String getTransportation() {
+        return transportation;
+    }
+
+    public int getMaxParticipants() {
+        return maxParticipants;
+    }
+
+    public double getBaseCost() {
+        return baseCost;
+    }
+
+    public double getOneBmMemberCost() {
+        return oneBmMemberCost;
+    }
+
+    public double getTwoBmMemberCost() {
+        return twoBmMemberCost;
+    }
+
+    public boolean isTaxDeductable() { return taxDeductable; }
 }
