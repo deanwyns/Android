6b8c8833dc9371b00c823175e0e9adb1089533f9
JaimySmets <jaimy.smets.s0645@student.hogent.be>
2014-11-03 11:02:56 +0100

    Code aanvulling toon kamp
    Meer informatie wordt nu getoond van toon kamp

--- app/src/main/java/hogent/hogentprojecteniii_groep10/SpecificVacationView.java
+++ app/src/main/java/hogent/hogentprojecteniii_groep10/SpecificVacationView.java
@@ -7,13 +7,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+
 import hogent.hogentprojecteniii_groep10.models.Vacation;
 
 
 public class SpecificVacationView extends Activity {
 
     private Vacation selectedVacation;
-    private TextView vacationTitleTextView, vacationDescriptionTextView, vacationPriceTextView;
+    private TextView vacationTitleTextView, vacationPromotionalTextTextView, vacationDescriptionTextView,
+            vacationDestinationTextView, vacationAgeTextView, vacationWhenTextView,
+            vacationTransportationTextView, vacationParticipantsTextView, vacationPriceTextView,
+            vacationDiscountPriceTextView, vacationTaxDeductableTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +26,32 @@ public class SpecificVacationView extends Activity {
         setContentView(R.layout.activity_specific_vacation_view);
 
         selectedVacation = (Vacation) getIntent().getParcelableExtra("SpecificVacation");
+
         vacationTitleTextView = (TextView) findViewById(R.id.specific_vacation_title);
+        vacationPromotionalTextTextView = (TextView) findViewById(R.id.specific_vacation_promotext_text);
         vacationDescriptionTextView = (TextView) findViewById(R.id.specific_vacation_description_text);
+        vacationDestinationTextView = (TextView) findViewById(R.id.specific_vacation_destination_text);
+        vacationAgeTextView = (TextView) findViewById(R.id.specific_vacation_age_text);
+        vacationWhenTextView = (TextView) findViewById(R.id.specific_vacation_when_text);
+        vacationTransportationTextView = (TextView) findViewById(R.id.specific_vacation_transportation_text);
+        vacationParticipantsTextView = (TextView) findViewById(R.id.specific_vacation_participants_text);
         vacationPriceTextView = (TextView) findViewById(R.id.specific_vacation_price_text);
+        vacationDiscountPriceTextView = (TextView) findViewById(R.id.specific_vacation_discount_price_text);
+        vacationTaxDeductableTextView = (TextView) findViewById(R.id.specific_vacation_tax_deductable_text);
 
         vacationTitleTextView.setText(selectedVacation.getTitle());
+        vacationPromotionalTextTextView.setText(selectedVacation.getPromoText());
         vacationDescriptionTextView.setText(selectedVacation.getDescription());
+        vacationDestinationTextView.setText(selectedVacation.getLocation());
+        vacationAgeTextView.setText(String.format("%d - %d", selectedVacation.getAgeFrom(), selectedVacation.getAgeTo()));
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        vacationWhenTextView.setText(String.format("%s - %s", formatter.format(selectedVacation.getBeginDate().getTime()), formatter.format(selectedVacation.getEndDate().getTime())));
+        vacationTransportationTextView.setText(selectedVacation.getTransportation());
+        vacationParticipantsTextView.setText(String.format("?/%d", selectedVacation.getMaxParticipants()));
         vacationPriceTextView.setText(String.format("€%.2f", selectedVacation.getBaseCost()));
+        vacationDiscountPriceTextView.setText(String.format("Eén ouder lid van Bond Moyson: €%.2f\nBeide ouders lid van Bond Moyson: €%.2f",
+                selectedVacation.getOneBmMemberCost(), selectedVacation.getTwoBmMemberCost()));
+        vacationTaxDeductableTextView.setText((selectedVacation.isTaxDeductable() ? "Ja" : "Nee"));
     }
 
 
